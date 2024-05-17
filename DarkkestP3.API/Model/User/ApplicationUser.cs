cc1d8568e0f63dd6fecadb01ac7458ec17366c3c
profunem(KungLo) <professionally_unemployed@proton.me>
2024-05-17 15:25:52 -0400

    Setup user login and registration

---
+++ DarkkestP3.API/Model/User/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Darkkest.API.Model;
+
+public class ApplicationUser : IdentityUser
+{
+    public bool IsOrganization { get; set; } = false;
+}
---
+++ DarkkestP3.API/Service/Interface/ServiceInterfaces.cs
@@ -0,0 +1,11 @@
+using Darkkest.API.DTO;
+using Microsoft.AspNetCore.Identity;
+
+namespace Darkkest.API.Service;
+
+public interface IUserService
+{
+    Task<IdentityResult> RegisterUser(RegisterUser registration);
+    Task<SignInResult> LoginUser(LoginUser login);
+    void Logout();
+}

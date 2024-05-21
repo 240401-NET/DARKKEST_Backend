d51949c1904d84bbb62aa949463e5e6d3bc135a1
sciwiz12 <ricardopenamcknight@gmail.com>
2024-05-21 15:30:33 -0400

    added Application DBset to context

--- DarkkestP3.API/Data/CommunityDBContext.cs
+++ DarkkestP3.API/Data/CommunityDBContext.cs
@@ -7,5 +7,6 @@ public class CommunityDBContext : DbContext
     public CommunityDBContext(DbContextOptions<CommunityDBContext> options) : base(options){}
 
     public virtual DbSet<Opportunity>  Opputunities{get; set;}
+    public virtual DbSet<Application>  Applications{get; set;}
     //public virtual DbSet<ModelName>  PluralName{get; set;}
 }
